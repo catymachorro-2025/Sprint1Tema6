f30f5cfb6a6c1572c9ef45a17a13a69d038f12dd
Lourdes Catalina Machorro <catymachorro@gmail.com>
2025-10-21 19:52:51 +0200

    LogicaSuperglobales corregido despues de ver que las variables no se guardaban

--- Ejercicio1/LogicaSuperglobales.php
+++ Ejercicio1/LogicaSuperglobales.php
@@ -9,5 +9,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_post = isset($_POST["email"]) ? $_POST["email"] : '';
     $contrasena_post = isset($_POST["contrasena"]) ? $_POST["contrasena"] : ''; 
 
+    $_SESSION["username"] = $username_post;
+    $_SESSION["email"] = $_SESSION["email"] = $email_post;
+    $_SESSION["contrasena"] = $contrasena_post;
+    
+  
 }
 ?>
