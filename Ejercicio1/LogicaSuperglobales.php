7fd91693ebcda9beae7eeb9b888783deb532dcf1
Lourdes Catalina Machorro <catymachorro@gmail.com>
2025-10-21 19:50:09 +0200

    Ejercicio 1 Validado con el Ejercicio2 del Sprint 7 requerido en el archivo LogicaSuperglobales.php

--- Ejercicio1/LogicaSuperglobales.php
+++ Ejercicio1/LogicaSuperglobales.php
@@ -1,17 +1,13 @@
 <?php 
+require_once 'C:\Users\Usuario\Documents\Curso PHP\Sprint1Tema7\Ejercicio2.php';
+
 
 session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $username_post = isset($_POST["username"]) ? $_POST["username"] : ''; 
     $email_post = isset($_POST["email"]) ? $_POST["email"] : '';
     $contrasena_post = isset($_POST["contrasena"]) ? $_POST["contrasena"] : ''; 
 
-    $_SESSION["username"] = $username_post;
-    $_SESSION["email"] = $_SESSION["email"] = $email_post;
-    $_SESSION["contrasena"] = $contrasena_post;
-    
-  
 }
 ?>
